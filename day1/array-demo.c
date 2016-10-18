e4eeabc5438c7cfbc51875ccbf22277aefc22ab6
Liam Echlin <wechlin@umbctraining.com>
2016-10-18 11:45:45 -0400

    Add array demo

---
+++ day1/array-demo.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	int grades[] = { 85, 85, 0, 95, 75, 85, 100, 85, 70, 75 };
+	int absents[] = { 1, 1, 9, 2, 0, 3, 4, 1, 0, 3 };
+
+	printf("grades[4] = %d\n", grades[4]);
+	printf("absents[0] = %d\n", absents[0]);
+}
