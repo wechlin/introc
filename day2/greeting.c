4a030faf5a68ae10dfbefb248ba96da74af272b1
Liam Echlin <wechlin@umbctraining.com>
2016-10-19 09:43:49 -0400

    Add array of ints demo and sizing

--- day2/greeting.c
+++ day2/greeting.c
@@ -5,6 +5,19 @@ int main(void)
 {
 	char greeting[] = "Hello, World";
 
+	char tedium[] = { 'H' };
+
+	int grades[] = {85, 95, 43, 77};
+
+	printf("%s\n", tedium);
 
 	printf("%s\n", greeting);
+
+	printf("%d\n", grades[2]);
+
+	printf("tedium: %zd\n", sizeof(tedium));
+	printf("greet: %zd\n", sizeof(greeting));
+
+	printf("grades: %zd\n", sizeof(grades));
+	
 }
