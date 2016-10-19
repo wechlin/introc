7b363e06e494ab175fc48e46c031453ccd56eaf6
Liam Echlin <wechlin@umbctraining.com>
2016-10-19 10:22:26 -0400

    Duplicate while loop in for loop

--- day2/loopy.c
+++ day2/loopy.c
@@ -4,12 +4,17 @@
 int main(void)
 {
 	int grades[] = { 75, 86, 99, 100, 67 };
-	int n = 0;
 	
+	int n = 0;
 	while(n < 5) {
 		printf("Grade %d is %d\n", n, grades[n]);
 
 		n += 1;
 	}
+
+	for(int n = 0; n < 5; n += 1) {
+		printf("Grade %d is %d\n", n, grades[n]);
+	}
+
 
 }
