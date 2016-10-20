b8bd69cef268a56fc22b6cc87df97e1720b8123d
Liam Echlin <wechlin@umbctraining.com>
2016-10-20 14:46:22 -0400

    Add global scope example

--- day3/scope.c
+++ day3/scope.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 
+int warming = 76;
+
 int iterator(void);
 
 int main(void)
@@ -14,14 +16,19 @@ int main(void)
 
 	value = iterator();
 	printf("%d\n", value);
+
+	printf("warming: %d\n", warming);
 }
 
 int iterator(void)
 {
 	static int x = 0;
 
+	printf(" in iter: %d\n", warming);
+
 	x += 1;
 
 	return x;
 }
+
 
