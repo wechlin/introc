3c1bfebd203e2597e42a6ce590fecf455f9db8a3
Liam Echlin <wechlin@umbctraining.com>
2016-10-20 14:33:50 -0400

    Demonstrate lack of saved or stored value

--- day3/scope.c
+++ day3/scope.c
@@ -8,11 +8,20 @@ int main(void)
 	int value = iterator();
 
 	printf("%d\n", value);
+
+	value = iterator();
+	printf("%d\n", value);
+
+	value = iterator();
+	printf("%d\n", value);
 }
 
 int iterator(void)
 {
-	int x = 1;
+	int x = 0;
+
+	x += 1;
 
 	return x;
 }
+
